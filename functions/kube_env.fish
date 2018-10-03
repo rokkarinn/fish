3d3366bafa3f182f29b0f5ad55d5718d3d096f5f
Ívar Björn Larsen <ivarbjorn@icelandair.is>
2018-10-03 16:14:06 +0000

    remove unused names

--- functions/kube_env.fish
+++ functions/kube_env.fish
@@ -1,5 +1,5 @@
 function kube_env
-  if string match -e $argv 'dev' 'stage' 'prod' 'test' 'capacity' 'central'
+  if string match -e $argv 'dev' 'stage' 'prod' 'central'
     set -gx KUBECONFIG "$HOME/.kube/$argv.icelandair"
   else
     echo "$argv is not an environmet I'm familiar with"
