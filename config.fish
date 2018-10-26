4f8115f7939e9671b5bb2f377a53ffd56e7b96b6
Ívar Björn Larsen <ivarbjorn@icelandair.is>
2018-10-26 08:54:37 +0000

    Fish changes specially for mac

--- config.fish
+++ config.fish
@@ -1,1 +1,2 @@
-set -g fish_user_paths "/usr/local/opt/gettext/bin" "/Users/ivar/Library/Python/3.7/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/gettext/bin" "/Users/ivar/Library/Python/3.7/bin" "/Library/Frameworks/Mono.framework/Versions/5.16.0/bin" $fish_user_paths
+set -gx JDK_HOME "/Library/Java/JavaVirtualMachines/openjdk-11.jdk/Contents/Home"
