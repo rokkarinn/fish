b8eb3e40470d09ed76a635c15bcf319e1b1f04ce
Ívar Björn Larsen <ivarbjorn@icelandair.is>
2018-10-26 16:01:39 +0000

    Update config.fish
    Teach vim to fish

--- config.fish
+++ config.fish
@@ -1,2 +1,3 @@
-set -g fish_user_paths "/usr/local/opt/gettext/bin" "/Users/ivar/Library/Python/3.7/bin" "/Library/Frameworks/Mono.framework/Versions/5.16.0/bin" $fish_user_paths
+set VSCODE_PATH "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+set -g fish_user_paths "/usr/local/opt/gettext/bin" "/Users/ivar/Library/Python/3.7/bin" "/Library/Frameworks/Mono.framework/Versions/5.16.0/bin" $VSCODE_PATH $fish_user_paths
 set -gx JDK_HOME "/Library/Java/JavaVirtualMachines/openjdk-11.jdk/Contents/Home"
